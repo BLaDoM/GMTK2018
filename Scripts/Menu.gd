6f106c98cb202236763a806f4b78eac4e097f223
Truc4 <curtreyes00@gmail.com>
2018-09-02 14:04:29 -0600

    juke fix

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,7 +1,21 @@
 extends Node
 
+var i = 3
+
+func _ready():
+	$Fade.set_visible(true)
+	$Fade.set_modulate(Color(0,0,0,0))
+
+func _process(delta):
+	if i < 2:
+		$Fade.set_modulate(Color(0,0,0,i))
+	$MenuMusic.volume_db = 0-i/20
+	i += .01
+	if i > 1 and i < 2:
+		get_tree().change_scene("res://Scenes/Controller.tscn")
+
 func _Play():
-	get_tree().change_scene("res://Scenes/Controller.tscn")
+	i = 0
 
 func _Credits():
 	get_tree().change_scene("res://Scenes/Credits.tscn")
