6c6da0b822076b2922423cd2613712a026d98be1
Truc4 <curtreyes00@gmail.com>
2018-09-02 06:35:33 -0600

    noprint

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -10,7 +10,6 @@ func _ready():
 func _process(delta):
 	$Sprite.set_modulate(Color(0,0,0, 1-fade))
 	$AudioStreamPlayer.volume_db = (fade - 1)*10
-	print(fade,is_fade)
 	if is_fade == true:
 		fade -= .01
 		if fade < .01:
@@ -22,4 +21,4 @@ func _on_Button_pressed():
 
 
 func _on_Button2_pressed():
-	print("Button2pressed")
+	pass
