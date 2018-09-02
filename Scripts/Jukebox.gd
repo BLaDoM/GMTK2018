6fa2c4e76c3befaf4844f3e238f3d0c94755fb5b
BLaDoM <simplybladom@gmail.com>
2018-09-02 12:45:11 -0300

    New Jukebox

--- Scripts/Jukebox.gd
+++ Scripts/Jukebox.gd
@@ -1,6 +1,22 @@
-extends AudioStreamPlayer
+extends Node
 
 var audioBuses = {} # Dictionary containing each bus and it's current target volume
+const volumes = [[0.0, 0.0, 0.0], [-100.0, 0.0, 0.0], [-100.0, -100.0, 0.0], [-100.0, - 100.0, -100.0]]
+var t = 0.0
 
 func _ready():
-	audioBuses = Audio
+	audioBuses = {
+		get_node("AudioStreamPlayer"):0.0,
+		get_node("AudioStreamPlayer2"):0.0,
+		get_node("AudioStreamPlayer3"):0.0}
+
+func _process(delta):
+	for i in range(len(audioBuses)):
+		audioBuses.keys()[i].volume_db = lerp(audioBuses.keys()[i].volume_db, audioBuses.values()[i], delta * 0.1)
+
+func updateVolumes(healthPercentage):
+	var i = floor(float(healthPercentage) / 4)
+	audioBuses = {
+		get_node("AudioStreamPlayer"):volumes[i][0],
+		get_node("AudioStreamPlayer2"):volumes[i][1],
+		get_node("AudioStreamPlayer3"):volumes[i][2]}
