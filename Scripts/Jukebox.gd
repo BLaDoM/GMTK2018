bd7b8cace6836de2eecf9fef9b3780641bdd703b
Truc4 <curtreyes00@gmail.com>
2018-09-02 10:23:21 -0600

    jukebox broke

--- Scripts/Jukebox.gd
+++ Scripts/Jukebox.gd
@@ -1,22 +1,24 @@
 extends Node
 
 var audioBuses = {} # Dictionary containing each bus and it's current target volume
-const volumes = [[0.0, 0.0, 0.0], [-100.0, 0.0, 0.0], [-100.0, -100.0, 0.0], [-100.0, - 100.0, -100.0]]
+const volumes = [[0.0, 0.0, 0.0], [-100.0, 0.0, 0.0], [-100.0, -100.0, 0.0], [-100.0, - 100.0, -100.0], [-100.0, - 100.0, -100.0]]
 var t = 0.0
 
 func _ready():
 	audioBuses = {
 		get_node("AudioStreamPlayer"):0.0,
 		get_node("AudioStreamPlayer2"):0.0,
-		get_node("AudioStreamPlayer3"):0.0}
+		get_node("AudioStreamPlayer3"):0.0,
+		get_node("AudioStreamPlayer4"):0.0}
 
 func _process(delta):
 	for i in range(len(audioBuses)):
 		audioBuses.keys()[i].volume_db = lerp(audioBuses.keys()[i].volume_db, audioBuses.values()[i], delta * 0.1)
 
 func updateVolumes(healthPercentage):
-	var i = floor(float(healthPercentage) / 4)
+	var i = ceil(float(healthPercentage) / 4) * 4
 	audioBuses = {
 		get_node("AudioStreamPlayer"):volumes[i][0],
 		get_node("AudioStreamPlayer2"):volumes[i][1],
-		get_node("AudioStreamPlayer3"):volumes[i][2]}
+		get_node("AudioStreamPlayer3"):volumes[i][2],
+		get_node("AudioStreamPlayer4"):volumes[i][3]}
